3b3e5ae943686fb4835791d129d03f0e172a716a
Slonov Boris <sslonovborisss@gmail.com>
2025-05-28 15:17:33 +0700

    change address

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,82 +1,76 @@
 import React from "react";
-import styles from "./style.module.css"
+import styles from "./style.module.css";
 import LogoIcon from "@/shared/ui/icons/LogoIcon";
 import clsx from "clsx";
 import Socials from "../Socials";
 import Link from "next/link";
 
 const Footer = () => {
+  const contacts = [
+    { text: "+7(812)900-50-51", link: "tel:+7(812)900-50-51" },
+    { text: "edu@vibro-laser.com", link: "mailto:edu@vibro-laser.com" },
+  ];
 
-    const contacts = [
-        { text: '+7(812)900-50-51', link: 'tel:+7(812)900-50-51' },
-        { text: 'edu@vibro-laser.com', link: 'mailto:edu@vibro-laser.com' }
-    ]
-
-    return (
-        <footer className={styles.footer} id="contacts">
-            <div className="container">
-                <div className={styles.body}>
-                    <div className={clsx(styles.item, styles.item1)}>
-                        <LogoIcon className={styles.logo} />
-                    </div>
-                    <div className={clsx(styles.item, styles.item2)}>
-                        <ul className={styles.menu}>
-                            <li className={styles.menuLi}>
-                                <a className={styles.a} href="#laser-alignment">
-                                    Выверка центровки
-                                </a>
-                            </li>
-                            <li className={styles.menuLi}>
-                                <a className={styles.a} href="#geometry-alignment">
-                                    Выверка геометрии
-                                </a>
-                            </li>
-                            <li className={styles.menuLi}>
-                                <a className={styles.a} href="#vr">
-                                    VR
-                                </a>
-                            </li>
-                            <li className={styles.menuLi}>
-                                <a className={styles.a} href="#vibration-diagnostics">
-                                    Вибродиагностика
-                                </a>
-                            </li>
-                            <li className={styles.menuLi}>
-                                <a className={styles.a} href="#foundation-support">
-                                    Поддержка фонда
-                                </a>
-                            </li>
-                        </ul>
-                        <div className={styles.contactsWrapper}>
-                            <div className={styles.contacts}>
-                                <h3 className={styles.h3}>
-                                    Адрес
-                                </h3>
-                                <p className={styles.address}>
-                                    Санкт-Петербург,
-                                    ул. Шпалерная, 22
-                                </p>
-                            </div>
-                            <div className={styles.contacts}>
-                                <h3 className={styles.h3}>
-                                    Контакты для связи
-                                </h3>
-                                <ul className={styles.ul}>
-                                    {contacts.map((item, index) => (
-                                        <li className={styles.li} key={index}>
-                                            <Link className={styles.a} href={item.link}>
-                                                {item.text}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer className={styles.footer} id="contacts">
+      <div className="container">
+        <div className={styles.body}>
+          <div className={clsx(styles.item, styles.item1)}>
+            <LogoIcon className={styles.logo} />
+          </div>
+          <div className={clsx(styles.item, styles.item2)}>
+            <ul className={styles.menu}>
+              <li className={styles.menuLi}>
+                <a className={styles.a} href="#laser-alignment">
+                  Выверка центровки
+                </a>
+              </li>
+              <li className={styles.menuLi}>
+                <a className={styles.a} href="#geometry-alignment">
+                  Выверка геометрии
+                </a>
+              </li>
+              <li className={styles.menuLi}>
+                <a className={styles.a} href="#vr">
+                  VR
+                </a>
+              </li>
+              <li className={styles.menuLi}>
+                <a className={styles.a} href="#vibration-diagnostics">
+                  Вибродиагностика
+                </a>
+              </li>
+              <li className={styles.menuLi}>
+                <a className={styles.a} href="#foundation-support">
+                  Поддержка фонда
+                </a>
+              </li>
+            </ul>
+            <div className={styles.contactsWrapper}>
+              <div className={styles.contacts}>
+                <h3 className={styles.h3}>Адрес</h3>
+                <p className={styles.address}>
+                  191123, г. Санкт-Петербург, Захарьевская ул., дом 10 литера В
+                </p>
+              </div>
+              <div className={styles.contacts}>
+                <h3 className={styles.h3}>Контакты для связи</h3>
+                <ul className={styles.ul}>
+                  {contacts.map((item, index) => (
+                    <li className={styles.li} key={index}>
+                      <Link className={styles.a} href={item.link}>
+                        {item.text}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-        </footer>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Sections/NewApp/index.tsx
+++ src/components/Sections/NewApp/index.tsx
@@ -1,63 +1,73 @@
-import Image from "next/image"
-import styles from "./styles.module.scss"
-import Link from "next/link"
-import { InViewStyle } from "@/shared/ui/InViewStyle"
+import Image from "next/image";
+import styles from "./styles.module.scss";
+import Link from "next/link";
+import { InViewStyle } from "@/shared/ui/InViewStyle";
 
 const NewApp = () => {
-
-    return (
-        <section className={styles.section}>
-            <div className="container">
-                <div className={styles.body}>
-                    <div className={styles.item}>
-                        <InViewStyle
-                            initialClass="leftToRight"
-                            animationClass="visible"
-                            triggerOnce
-                        >
-                            <Image className={styles.img} src={'/app.png'} alt={'скриншот приложения'} width={510} height={330} />
-                        </InViewStyle>
-                    </div>
-                    <div className={styles.item}>
-                        <h2 className={styles.h2}>
-                            <InViewStyle
-                                initialClass="bottomToTop"
-                                animationClass="visible"
-                                triggerOnce
-                            >
-                                Новое приложение
-                            </InViewStyle>
-                        </h2>
-                        <div className={styles.p}>
-                            <InViewStyle
-                                initialClass="bottomToTop"
-                                animationClass="visible"
-                                triggerOnce
-                            >
-                                Революционный интуитивно понятный интерфейс с 3D визуализацией
-                            </InViewStyle>
-                        </div>
-                        <InViewStyle
-                            initialClass="rightToLeft"
-                            animationClass="visible"
-                            triggerOnce
-                        >
-                            <div className={styles.links}>
-                                <p className={styles.a} >
-                                    <Image src={'/android.svg'} alt="плейстор" width={25} height={25} />
-                                    для Android
-                                </p>
-                                <p className={styles.a} >
-                                    <Image src={'/ios.svg'} alt="плейстор" width={25} height={25} />
-                                    для iOS
-                                </p>
-                            </div>
-                        </InViewStyle>
-                    </div>
-                </div>
+  return (
+    <section className={styles.section}>
+      <div className="container">
+        <div className={styles.body}>
+          <div className={styles.item}>
+            <InViewStyle
+              initialClass="leftToRight"
+              animationClass="visible"
+              triggerOnce
+            >
+              <Image
+                className={styles.img}
+                src={"/app.png"}
+                alt={"скриншот приложения"}
+                width={510}
+                height={330}
+              />
+            </InViewStyle>
+          </div>
+          <div className={styles.item}>
+            <h2 className={styles.h2}>
+              <InViewStyle
+                initialClass="bottomToTop"
+                animationClass="visible"
+                triggerOnce
+              >
+                Новое приложение
+              </InViewStyle>
+            </h2>
+            <div className={styles.p}>
+              <InViewStyle
+                initialClass="bottomToTop"
+                animationClass="visible"
+                triggerOnce
+              >
+                Революционный интуитивно понятный интерфейс с 3D визуализацией
+              </InViewStyle>
             </div>
-        </section>
-    )
-}
+            <InViewStyle
+              initialClass="rightToLeft"
+              animationClass="visible"
+              triggerOnce
+            >
+              <div className={styles.links}>
+                <a
+                  href="https://play.google.com/store/apps/details?id=com.VibroLaser.Base"
+                  target="_blank"
+                  className={styles.a}
+                >
+                  <Image
+                    src={"/android.svg"}
+                    alt="плейстор"
+                    width={25}
+                    height={25}
+                  />
+                  для Android
+                </a>
+              </div>
+            </InViewStyle>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default NewApp
+export default NewApp;
